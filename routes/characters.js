09e6e10a9650df9be38fe4371fc5c2c314fd9267
HOUHOU <houhou@MacBook-Pro-de-HOUHOU.local>
2024-02-19 09:29:06 +0100

    motif-back-characterid

--- routes/characters.js
+++ routes/characters.js
@@ -26,5 +26,18 @@ router.get("/characters", async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
+router.get("/character/:characterId", async (req, res) => {
+  try {
+    // ex. de params: 5fcf91f4d8a2480017b91453
+    const { characterId } = req.params;
+    const { data } = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    // console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
